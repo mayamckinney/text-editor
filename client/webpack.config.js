2ac467dc9182010396dd8fdd4a11e2d7b953fc0d
mayamckinney <lleannastephenss@gmail.com>
2023-02-21 14:17:48 -0700

    webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,48 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest:'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A downloadable text editor for you to use on or offline!',
+        start_url: './',
+        fingerprints: true,
+        inject: true,
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        publicPath: './',
+        icons: [{
+          src: path.resolve('./images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\/m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
